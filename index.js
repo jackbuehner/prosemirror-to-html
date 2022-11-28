e2d294544f012d2bfe432c00ccca0989ef134015
Jack Buehner <git@jackbuehner.com>
2022-11-28 00:31:54 -0500

    feat: provide typescript definitions based on jsdoc comments

--- index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  Renderer: require('./src/Renderer'),
-  Node: require('./src/Nodes/Node'),
-  Mark: require('./src/Marks/Mark'),
-};
---
+++ src/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  /**
+   * @type {typeof import('./Renderer')}
+   */
+  Renderer: require('./Renderer'),
+  /**
+   * @type {typeof import('./Nodes/Node')}
+   */
+  Node: require('./Nodes/Node'),
+  /**
+   * @type {typeof import('./Marks/Mark')}
+   */
+  Mark: require('./Marks/Mark'),
+};
